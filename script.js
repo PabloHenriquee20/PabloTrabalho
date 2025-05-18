77abe4bc472902ff29f21b4a737dfe7f0f578148
PabloHenriquee20 <pablo.henriquegg@outlook.com>
2025-05-18 17:10:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+  const div = document.getElementById("mensagem");
+  div.innerHTML = "<p><strong>Seja Bem vindo! Não sei quantas lutas vamos passar, mas DEUS vai nos capacitar para tudo e para todos! 🌟</strong></p>";
+}
